b8e48c082331d0212d4ad906e14a4d5dbabaf1d1
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-01-21 11:43:27 -0300

    correções navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-function menuShow() {
+function menuShowIndex() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
@@ -6,5 +6,16 @@ function menuShow() {
     } else {
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "img/close_white_36dp.svg";
+    }
+}
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "../img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "../img/close_white_36dp.svg";
     }
 }
